145fdf56b137fd441c4b09d40bb986a13cc097b4
ItsKingzman <connerk4@gmail.com>
2022-10-18 16:11:50 -0500

    Fixed some things

--- src/routes/fun/m1/+page.svelte
+++ src/routes/fun/m1/+page.svelte
@@ -2,18 +2,15 @@
 	<div class="flex items-center justify-center w-full h-full">
 		<div class="relative flex h-[400px] w-96">
 			<div
-				class="absolute z-10 flex items-center justify-center w-full h-full bg-gradient-to-br from-gray-900 to-black"
-			>
+				class="absolute z-10 flex items-center justify-center w-full h-full bg-gradient-to-br from-gray-900 to-black">
 				<h1
-					class="-mt-2 font-bold text-transparent bg-clip-text bg-gradient-to-br from-gray-600 to-white text-8xl"
-				>
+					class="-mt-2 font-bold text-transparent bg-clip-text bg-gradient-to-br from-gray-600 to-white text-8xl">
 					CK
 				</h1>
 			</div>
 			<div class="absolute w-full h-full bg-conic-gradient filter blur-xl" />
 			<div
-				class="absolute w-full h-full bg-conic-gradient filter blur-3xl opacity-60 animate-pulse"
-			/>
+				class="absolute w-full h-full bg-conic-gradient filter blur-3xl opacity-60 animate-pulse"/>
 			<div class="absolute -inset-0.5 rounded-sm bg-conic-gradient" />
 		</div>
 	</div>
