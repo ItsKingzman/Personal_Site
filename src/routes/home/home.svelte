7d9df9fd7ae105c273547524b2acf2fde13562b0
ItsKingzman <connerk4@gmail.com>
2022-11-08 15:43:50 -0600

    Small update

--- src/routes/home/home.svelte
+++ src/routes/home/home.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-col items-center justify-center h-screen text-5xl text-center cursor-crosshair select-none">
+<div class="flex flex-col items-center justify-center h-screen text-5xl text-center select-none">
 	<div class="flex items-center justify-center py-2">
 		&#8249
 		<div class="w-40 opti-01 h-[2px] mt-2 mx-6 rounded" />
